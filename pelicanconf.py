abddf3918e6e29123482a93043b08426fe162792
Ben Healy <peterbenjamin.healy@gmail.com>
2014-10-31 10:24:28 -0400

    updated crowsgoot theme
    uploading the updataed crowsfoot theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,7 +43,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME = "pelican-themes/crowsfoot"
+THEME = "pelican-themes/crowsfoot-master"
 
 DISPLAY_CATEGORIES_ON_MENU = False
 
